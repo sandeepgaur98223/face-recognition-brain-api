0eb668a5a79b059170bfb05dd4444bc73fc93e2f
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-03-23 20:08:34 +0530

    first one

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send('This is working...');
+})
+
+app.listen(3000,()=>{
+    console.log("app is running on Port 3000")
+})
