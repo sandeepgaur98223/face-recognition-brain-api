ae47e3d21ca57cdf43d9d40b22f4ec975b5b26e4
sandeepgaur98223 <sandeepgaur98223@gmail.com>
2023-03-25 19:02:06 +0530

    done with the count

--- server.js
+++ server.js
@@ -52,7 +52,8 @@ app.post('/signin',(req,res)=>{
     if(req.body.email===database.users[0].email &&
         req.body.password===database.users[0].password)
         {
-            res.json("success");
+           // res.json("success");
+           res.json(database.users[0])
         }
         else
         {
